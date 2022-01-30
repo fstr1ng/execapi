55e113ff430aa8ba689fa10bfb1c434491b71b95
Mikhail Fedoseev <blackglasscat@gmail.com>
2022-01-30 20:18:50 +0300

    Add get_entities_from_db function

--- app/routes.py
+++ app/routes.py
@@ -7,18 +7,19 @@ from app import app, db
 from app.models import Task, Host
 from app.tasks import create_exec_task, callback_update_exec, create_host_task
 
+def get_entities_from_db(database_class):
+    all_entities = database_class.query.all()
+    all_entities_dict = [entity.__dict__ for entity in all_entities]
+        for entity in all_entities_dict:
+            del entity['_sa_instance_state']
+    return all_entities_dict
 
 @app.route('/host', methods=['GET','POST'])
 def host_all_endpoint(name=None):
     ''' GET returns all hosts
         POST creates new host '''
     if request.method=='GET':
-        all_hosts = Host.query.all()
-        #pdb.set_trace()
-        all_hosts_dicts = [host.__dict__ for host in all_hosts]
-        for host in all_hosts_dicts:
-            del host['_sa_instance_state']
-        return jsonify(all_hosts_dicts)
+        return jsonify(get_entities_from_db(Host))
     if request.method=='POST':
         data = request.get_json()
         celery_task = create_host_task.apply_async(
@@ -44,26 +45,30 @@ def host_one_endpoint(name):
                 database_host.__setattr__(key, data[key])
                 host_dict = database_host.__dict__
                 del host_dict['_sa_instance_state']
+                db.session.commit()
                 return jsonify(host_dict)
             except AttributeError:
                 return {'error': 'no such attribute'}
 
-
 @app.route('/exec', methods=['GET', 'POST'])
 def exec_endpoint():
-    data = request.get_json()
-    task_id = uuid.uuid4().hex
-    database_task = Task(
-        id=task_id,
-        command=data['command'],
-        )
-    db.session.add(database_task)
-    db.session.commit()
-    celery_task = create_exec_task.apply_async(
-            (task_id, data['host'], data['command']),
-            task_id=task_id,
-            link=callback_update_exec.s(task_id,'done'),
-            link_error=callback_update_exec.s(task_id, 'failed')
+    ''' GET returns all exec tasks
+        POST creates new exec task '''
+    if request.method=='GET':
+        return jsonify(get_entities_from_db(Task))
+    if request.method=='POST':
+        data = request.get_json()
+        task_id = uuid.uuid4().hex
+        database_task = Task(
+            id=task_id,
+            command=data['command'],
             )
-    return data
-
+        db.session.add(database_task)
+        db.session.commit()
+        celery_task = create_exec_task.apply_async(
+                (task_id, data['host'], data['command']),
+                task_id=task_id,
+                link=callback_update_exec.s(task_id,'done'),
+                link_error=callback_update_exec.s(task_id, 'failed')
+                )
+        return data
