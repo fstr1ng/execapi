21a49fa3fe949b4fe9093aa2018926b2040d7c95
Mikhail Fedoseev <blackglasscat@gmail.com>
2022-01-30 20:06:48 +0300

    Separate host(s) function

--- app/routes.py
+++ app/routes.py
@@ -7,25 +7,49 @@ from app import app, db
 from app.models import Task, Host
 from app.tasks import create_exec_task, callback_update_exec, create_host_task
 
+
 @app.route('/host', methods=['GET','POST'])
-@app.route('/host/<name>', methods=['GET','POST'])
-def host_endpoint(name=None):
-    if not name and request.method=='GET':
+def host_all_endpoint(name=None):
+    ''' GET returns all hosts
+        POST creates new host '''
+    if request.method=='GET':
         all_hosts = Host.query.all()
         #pdb.set_trace()
         all_hosts_dicts = [host.__dict__ for host in all_hosts]
         for host in all_hosts_dicts:
             del host['_sa_instance_state']
         return jsonify(all_hosts_dicts)
-    if not name and request.method=='POST':
+    if request.method=='POST':
         data = request.get_json()
         celery_task = create_host_task.apply_async(
             (data['name'], data['address'], data['user']))
         return celery_task.task_id, data
 
+@app.route('/host/<name>', methods=['GET','POST'])
+def host_one_endpoint(name):
+    ''' GET returns exact host
+        POST edits existing host '''
+    if request.method=='GET':
+        database_host = Host.query.filter_by(name=name).first()
+        host_dict = database_host.__dict__
+        del host_dict['_sa_instance_state']
+        return jsonify(host_dict)
+    if request.method=='POST':
+        database_host = Host.query.filter_by(name=name).first()
+        data = request.get_json()
+        pdb.set_trace()
+        for key in data:
+            try:
+                database_host.__getattribute__(key)
+                database_host.__setattr__(key, data[key])
+                host_dict = database_host.__dict__
+                del host_dict['_sa_instance_state']
+                return jsonify(host_dict)
+            except AttributeError:
+                return {'error': 'no such attribute'}
 
 
-@app.route('/exec', methods=['POST'])
+@app.route('/exec', methods=['GET', 'POST'])
 def exec_endpoint():
     data = request.get_json()
     task_id = uuid.uuid4().hex
@@ -42,3 +66,4 @@ def exec_endpoint():
             link_error=callback_update_exec.s(task_id, 'failed')
             )
     return data
+
